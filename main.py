6f37f128977dea77ee934c74ed3b8fa307fd08cd
Tyrbelin <36834540+Tyrbelin@users.noreply.github.com>
2024-01-19 02:17:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,27 +19,26 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         if message.author == self.user:
             return
-        
-        if message.content:
-            print(message.content)
-        else:
-            print("메시지 내용 없음")
             
         if message.content.startswith("http://x.com/"):
             await message.delete()
             await message.channel.send('{0.author.mention} https://vxtwitter.com/'.format(message) + message.content[13:])
-            
+            return
+        
         elif message.content.startswith("https://x.com/"):
             await message.delete()
             await message.channel.send('{0.author.mention} https://vxtwitter.com/'.format(message) + message.content[14:])
+            return
             
         elif message.content.startswith("http://twitter.com/"):
             await message.delete()
             await message.channel.send('{0.author.mention} https://vxtwitter.com/'.format(message) + message.content[19:])
+            return
             
         elif message.content.startswith("https://twitter.com/"):
             await message.delete()
             await message.channel.send('{0.author.mention} https://vxtwitter.com/'.format(message) + message.content[20:])
+            return
      
 # Run Flask in a separate thread
 flask_thread = Thread(target=run_flask)
