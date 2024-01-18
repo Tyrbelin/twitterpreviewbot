377dbe0c07259c3fcbe5c846b99399187de41382
Tyrbelin <36834540+Tyrbelin@users.noreply.github.com>
2024-01-19 00:38:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import discord
 import os
- 
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/health', methods=['GET'])
+def health_check():
+    return '', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
 TOKEN = os.environ.get(f"DISCORD_TOKEN").strip()
 print('token : ' + TOKEN)
 
